1cc8ab372e998bc962bdae87a1cb991ce1b102a0
Mari Carmen Gutiérrez <valakirka@gmail.com>
2012-10-11 14:44:48 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crank::Application.config.secret_token = '9348d53cba92e81de8e07be90bcb3aa21cfac619ec29b3f7493ef857202beb4950e11c7fe18dbe34d78c09742bc4b937c15b21b15d36b44b7eaa9d88a8203482'
